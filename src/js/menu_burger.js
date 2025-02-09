7ae7d4b9452bdb1b95f575aaf70b01c9247939f4
Florian LABADIE <florian.labadie@epitech.eu>
2025-02-09 11:09:48 +0100

    refacto: refont

--- src/js/menu_burger.js
+++ src/js/menu_burger.js
@@ -5,3 +5,8 @@ const nav = document.querySelector('.nav-links');
 burger.addEventListener('click', () => {
     nav.classList.toggle('active');
 });
+
+document.getElementById("toggleCvBtn").addEventListener("click", function() {
+    let cvContainer = document.getElementById("cvContainer");
+    cvContainer.style.display = (cvContainer.style.display === "none" || cvContainer.style.display === "") ? "block" : "none";
+});
